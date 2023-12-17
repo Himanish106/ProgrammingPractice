63ea3520377f9c4d2693e1b44a07ab3ddf81547e
Himanish106 <himanish.das23@gmail.com>
2023-12-17 21:05:00 +0530

    commit file

--- Javascript/Ajax/ajax.js
+++ Javascript/Ajax/ajax.js
@@ -1,7 +1,23 @@
 console.log("This is ajax practice");
 let fetchBtn = document.getElementById("fetchBtn");
-fetchBtn.addEventListener('click',buttonClickHandler)
+fetchBtn.addEventListener("click", buttonClickHandler);
 
-function buttonClickHandler(){
-    // Instantiate an xhr object
+function buttonClickHandler() {
+  // Instantiate an xhr object
+  const xhr = new XMLHttpRequest();
+
+  // open the object
+  xhr.open("GET", "himanish.txt", true);
+
+  // What to do on progress(Optional)
+  xhr.onprogress = function () {
+    console.log("On Progress");
+  };
+
+  //   What to do when response will be ready
+  xhr.onload = function () {
+    console.log(this.responseText);
+  };
+  // Send the generated response as a request to the server
+  xhr.send();
 }
