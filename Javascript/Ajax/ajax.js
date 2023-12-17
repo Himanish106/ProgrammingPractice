5c94dbf3f9e3a0c1fec3264f7ad83921d93891ed
Himanish106 <himanish.das23@gmail.com>
2023-12-17 21:04:39 +0530

    commit

--- Javascript/Ajax/ajax.js
+++ Javascript/Ajax/ajax.js
@@ -3,5 +3,5 @@ let fetchBtn = document.getElementById("fetchBtn");
 fetchBtn.addEventListener('click',buttonClickHandler)
 
 function buttonClickHandler(){
-    // Instatiate an xhr object
+    // Instantiate an xhr object
 }
