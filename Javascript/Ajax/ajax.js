731def8a895838281a60ec1dcbb34e37216d3058
Himanish106 <himanish.das23@gmail.com>
2023-12-17 20:01:30 +0530

    ajax practice

---
+++ Javascript/Ajax/ajax.js
@@ -0,0 +1,7 @@
+console.log("This is ajax practice");
+let fetchBtn = document.getElementById("fetchBtn");
+fetchBtn.addEventListener('click',buttonClickHandler)
+
+function buttonClickHandler(){
+    // Instatiate an xhr object
+}
