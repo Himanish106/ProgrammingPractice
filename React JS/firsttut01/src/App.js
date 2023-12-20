373a15214a059122a67aa79a81117a3e430593fd
Himanish106 <himanish.das23@gmail.com>
2023-12-20 11:53:16 +0530

    commit

--- React JS/firsttut01/src/App.js
+++ React JS/firsttut01/src/App.js
@@ -1,53 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Navbar from "./Components/Navbar";
 function App() {
+  // return <Navbar />; ---> Suppose if we want to reuse this navbar for a different website obviously some things need to be changed such as "name of the website" "about of the website" etc. So here the concept of props comes. props means properties. We can pass the required properties and accordingly the properties will change. Remember props are always read only.
   return (
-    <>
-      <nav className="navbar navbar-expand-lg bg-body-tertiary">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#">
-           Text Utils
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon" />
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">
-                  Home
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  About
-                </a>
-              </li>
-              
-            </ul>
-            <form className="d-flex" role="search">
-              <input
-                className="form-control me-2"
-                type="search"
-                placeholder="Search"
-                aria-label="Search"
-              />
-              <button className="btn btn-outline-success" type="submit">
-                Search
-              </button>
-            </form>
-          </div>
-        </div>
-      </nav>
-    </>
+  <Navbar pageTitle="TextUtils" about="About TextUtils" /> // Since in Navbar.js You have already set the type of the properties mentioned here as a string so if you provide any other data type other than string then you will get an error
+  // <Navbar/> ---> Case to check for the default props
   );
 }
 
---
+++ React JS/firsttut01/src/Components/Navbar.js
@@ -0,0 +1,70 @@
+import React from "react";
+import PropTypes from "prop-types"; // PropTypes means type of the property i.e. whether String,array etc
+export default function Navbar(props) {
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            {props.pageTitle}
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  {props.about}
+                </a>
+              </li>
+            </ul>
+            <form className="d-flex" role="search">
+              <input
+                className="form-control me-2"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+              />
+              <button className="btn btn-outline-success" type="submit">
+                Search
+              </button>
+            </form>
+          </div>
+        </div>
+      </nav>
+      {/* This was the concept of props. We are creating the navbar in this seperate js page so that it can be reused later. 
+Props in React are inputs that you pass into components. The props enable the component to access customised data, values, and pieces of information that the inputs hold. The term 'props' is an abbreviation for 'properties' which refers to the properties of an object. */}
+    </>
+  );
+}
+
+// Navbar.propTypes = {
+//   pageTitle: PropTypes.string,
+//   about: PropTypes.string,
+// };
+
+// isRequired attribute is used to set to make any props to be present mandatorily. Default prop would solve the propblem but in case if we forget to mention the default prop too is required attribute would resolve the problem
+Navbar.propTypes = {
+  pageTitle: PropTypes.string.isRequired,
+  about: PropTypes.string.isRequired,
+};
+
+// DEFAULT PROPS ---> This is used if we do not specify any props. Default value is replaced
+Navbar.defaultProps = {
+  pageTitle: "Set title Here",
+  about: "About Us",
+};
