4cf89b5c946a619f958bf41be71c90ccbfc0d87d
Himanish106 <himanish.das23@gmail.com>
2024-01-06 12:51:12 +0530

    Java Day 4

--- JavaRevise/src/JavaPrac/StringLinearSearch10.java
+++ JavaRevise/src/JavaPrac/StringLinearSearch10.java
@@ -5,10 +5,31 @@
  */
 package JavaPrac;
 
+import java.util.Scanner;
+
 /**
  *
  * @author hp
  */
 public class StringLinearSearch10 {
-    
+
+    public static void main(String[] args) {
+        String arr[] = {"Himanish", "Abhishek", "Mohit", "Rahul", "Rick", "Pratim"};
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a name to be searched:");
+        String target = sc.nextLine();
+        System.out.println(LinearSearchStrings(arr, target));
+    }
+
+    static String LinearSearchStrings(String arr[], String target) {
+        if (arr.length == 0) {
+            return "Array Empty";
+        }
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].equals(target)) {
+                return arr[i]+" was found at position " + i;
+            }
+        }
+        return "Element not found";
+    }
 }
