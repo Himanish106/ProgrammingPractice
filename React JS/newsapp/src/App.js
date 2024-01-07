3077016217c4f5c7d13346bc68858219672324c0
Himanish106 <himanish.das23@gmail.com>
2024-01-07 18:40:34 +0530

    React Day 7

--- React JS/newsapp/src/App.js
+++ React JS/newsapp/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import React, { Component } from 'react'
 import Navbar from './Components/Navbar';
 import News from './Components/News';
+import NewsFooter from './Components/NewsFooter';
 // c074793f71924ad5a65b35b29eb675d6
 export default class App extends Component {
   render() {
@@ -10,6 +11,7 @@ export default class App extends Component {
       <div>
         <Navbar />
         <News />
+        <NewsFooter/>
       </div>
     )
   }
---
+++ React JS/newsapp/src/Components/NewsFooter.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class NewsFooter extends Component {
+    render() {
+        return (
+            <div>  <footer className="bg-body-tertiary text-dark text-center py-3">
+                <p>© 2024 NewsMonkey. All rights reserved.</p>
+            </footer></div>
+
+        )
+    }
+}
