29b025c8284eb0cdc71d4e918864e22310f87b54
Himanish106 <himanish.das23@gmail.com>
2024-01-24 18:54:42 +0530

    Java Day 6
    Learned Concepts of Selection and Bubble Sort

---
+++ JavaRevise/src/JavaPrac/BubbleSort17.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package JavaPrac;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author Himanish
+ */
+public class BubbleSort17 {
+
+    public static void main(String[] args) {
+        int arr[] = {1, 2, 3, 4, 5};
+        bubbleSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void bubbleSort(int arr[]) {
+        boolean isSorted;
+//   Run the steps n-1 times
+        for (int i = 0; i < arr.length - 1; i++) {
+            System.out.println("No of passes:" + (i + 1));
+            isSorted = true;
+//    For each step the max item will come at the last respective index
+            for (int j = 1; j < arr.length - i; j++) {
+//    Swap if the item is smaller than the previous item
+                if (arr[j] < arr[j - 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = temp;
+                    isSorted = false;
+                }
+            }
+            if (isSorted) {
+                break;
+            }
+        }
+    }
+}
---
+++ JavaRevise/src/JavaPrac/SelectionSort18.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package JavaPrac;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author Himanish
+ */
+public class SelectionSort18 {
+
+    public static void main(String[] args) {
+        int arr[] = {5,4,3,2,1};
+        selectionSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void selectionSort(int arr[]) {
+        for (int i = 0; i < arr.length-1; i++) {
+            int lastIndex = arr.length - i - 1;
+            int maxIndexValue = maxIndex(arr, 0, lastIndex);
+            swap(arr, maxIndexValue, lastIndex);
+
+        }
+    }
+
+    static int maxIndex(int arr[], int startIndex, int lastIndex) {
+        int maxValue = startIndex;
+        for (int i = startIndex + 1; i <= lastIndex; i++) {
+            if (arr[maxValue] < arr[i]) {
+                maxValue = i;
+            }
+        }
+        return maxValue;
+    }
+
+    static void swap(int arr[], int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
