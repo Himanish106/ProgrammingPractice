070df6fcceb3beb706291d27a359224561be27ba
Himanish106 <himanish.das23@gmail.com>
2024-03-02 23:21:39 +0530

    Major Project Day1+Day2+Day3

--- Major Project Demo/event-management-app/src/App.js
+++ Major Project Demo/event-management-app/src/App.js
@@ -3,18 +3,20 @@ import About from "./Components/AboutBrief";
 import Contact from "./Components/ContactBrief";
 import IntroSection from "./Components/IntroSection";
 import Navbar from "./Components/Navbar";
+import ReviewSlider from "./Components/ReviewSlider";
 import Service from "./Components/ServiceBrief";
 import MySwiper from "./Components/Swiper";
 
 function App() {
   return (
     <>
-      <Navbar />;
+      <Navbar />
       <IntroSection />
       <MySwiper />
       <About />
       <Service />
       <Contact />
+      <ReviewSlider />
     </>
   );
 }
---
+++ Major Project Demo/event-management-app/src/Components/ReviewSlider.js
@@ -0,0 +1,123 @@
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "./CSS/ReviewSlider.css";
+import "../Global Files/global.css";
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import "swiper/css/effect-coverflow";
+import imgReview1 from "./Images/img1.jpg";
+import imgReview2 from "./Images/img2.jpg";
+import imgReview3 from "./Images/img3.jpg";
+import imgReview4 from "./Images/img4.jpg";
+import { Autoplay } from "swiper/modules";
+const ReviewSlider = () => {
+  return (
+    <section className="review" id="review">
+      <h1 className="heading">
+        client's <span className="col">review</span>
+      </h1>
+      <div className="review-slider swiper-container">
+        <Swiper
+          spaceBetween={5}
+          grabCursor={true}
+          slidesPerView={3}
+          loop={true}
+          breakpoints={{
+            0: {
+              slidesPerView: 1,
+            },
+            700: {
+              slidesPerView: 2,
+            },
+            1050: {
+              slidesPerView: 3,
+            },
+          }}
+          autoplay={{
+            delay: 800,
+            disableOnInteraction: false,
+          }}
+          modules={[Autoplay]}
+        >
+          <div className="swiper-wrapper">
+            <SwiperSlide>
+              <div className="swiper-slide box">
+                <i className="fas fa-quote-right" />
+                <div className="user">
+                  <img src={imgReview1} alt />
+                  <div className="user-info">
+                    <h3>nayana</h3>
+                    <span>happy customer</span>
+                  </div>
+                </div>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Corporis dolor dicta cum. Eos beatae eligendi, magni numquam
+                  nemo sed ut corrupti, ipsam iure nisi unde et assumenda
+                  perspiciatis voluptatibus nihil.
+                </p>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="swiper-slide box">
+                <i className="fas fa-quote-right" />
+                <div className="user">
+                  <img src={imgReview2} alt />
+                  <div className="user-info">
+                    <h3>lisa</h3>
+                    <span>happy customer</span>
+                  </div>
+                </div>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Corporis dolor dicta cum. Eos beatae eligendi, magni numquam
+                  nemo sed ut corrupti, ipsam iure nisi unde et assumenda
+                  perspiciatis voluptatibus nihil.
+                </p>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="swiper-slide box">
+                <i className="fas fa-quote-right" />
+                <div className="user">
+                  <img src={imgReview3} alt />
+                  <div className="user-info">
+                    <h3>mary</h3>
+                    <span>happy customer</span>
+                  </div>
+                </div>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Corporis dolor dicta cum. Eos beatae eligendi, magni numquam
+                  nemo sed ut corrupti, ipsam iure nisi unde et assumenda
+                  perspiciatis voluptatibus nihil.
+                </p>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="swiper-slide box">
+                <i className="fas fa-quote-right" />
+                <div className="user">
+                  <img src={imgReview4} alt />
+                  <div className="user-info">
+                    <h3>rose</h3>
+                    <span>happy customer</span>
+                  </div>
+                </div>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Corporis dolor dicta cum. Eos beatae eligendi, magni numquam
+                  nemo sed ut corrupti, ipsam iure nisi unde et assumenda
+                  perspiciatis voluptatibus nihil.
+                </p>
+              </div>
+            </SwiperSlide>
+          </div>
+        </Swiper>
+      </div>
+    </section>
+  );
+};
+export default ReviewSlider;
