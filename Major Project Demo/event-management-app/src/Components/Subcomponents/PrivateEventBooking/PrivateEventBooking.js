efd665f64518d6c6c457ab893cfaf68795629804
Himanish106 <himanish.das23@gmail.com>
2024-04-03 03:42:48 +0530

    PrivateEventBookingPage Completed

--- Major Project Demo/event-management-app/src/Components/Subcomponents/PrivateEventBooking/PrivateEventBooking.js
+++ Major Project Demo/event-management-app/src/Components/Subcomponents/PrivateEventBooking/PrivateEventBooking.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import eventTypesData from "../../../Global Files/privateevent.json";
 import venueData from "../../../Global Files/statesandlocations.json";
 import "../../CSS/PrivateEventBooking.css";
-import privateImg from "../../Images/PrivateEventLogo.jpg"
+import privateImg from "../../Images/PrivateEventLogo.jpg";
 const PrivateEventBooking = () => {
   const eventTypes = eventTypesData.eventTypes;
   const [selectedState, setSelectedState] = useState("");
@@ -18,6 +18,9 @@ const PrivateEventBooking = () => {
   const [totalPrice, setTotalPrice] = useState(
     selectedVenue ? selectedVenue.price1 : 0
   );
+  const [eventDescription, setEventDescription] = useState("");
+  const [selectedDate, setSelectedDate] = useState("");
+  const [isFormValid, setIsFormValid] = useState(false);
   const handleStateChange = (event) => {
     setSelectedState(event.target.value);
     setSelectedCity("");
@@ -34,17 +37,22 @@ const PrivateEventBooking = () => {
     const venues = venueData[selectedState][selectedCity];
     setSelectedVenue(venues[venueIndex]);
     calculatedTotalPrice();
+    setCateringFacility("");
+    setdesignAndPhotoFacility("");
+    checkFormValidity();
   };
   const handleCateringChange = (event) => {
     setCateringFacility(event.target.value);
     setSelectedCaterer("");
     calculatedTotalPrice();
+    checkFormValidity();
   };
   const handleDesignandPhotoServiceChange = (event) => {
     setdesignAndPhotoFacility(event.target.value);
     setSelectedDesignServices("");
     setSelectedPhotoVideoServices("");
     calculatedTotalPrice();
+    checkFormValidity();
   };
   const handleCapacityChange = (event) => {
     const enteredCapacity = parseInt(event.target.value);
@@ -54,6 +62,7 @@ const PrivateEventBooking = () => {
     }
     setCapacity(enteredCapacity);
     calculatedTotalPrice();
+    checkFormValidity();
   };
   const calculatedTotalPrice = () => {
     if (selectedVenue) {
@@ -73,13 +82,52 @@ const PrivateEventBooking = () => {
       setTotalPrice(totalPrice);
     }
   };
+  const handleDateChange = (event) => {
+    setSelectedDate(event.target.value);
+  };
+  const checkFormValidity = () => {
+    if (
+      selectedState &&
+      selectedCity &&
+      selectedVenue &&
+      cateringFacility &&
+      designAndPhotoFacility &&
+      capacity &&
+      eventDescription &&
+      selectedDate &&
+      (cateringFacility === "no" || selectedCaterer) &&
+      (designAndPhotoFacility === "no" ||
+        (selectedDesignServices && selectedPhotoVideoServices))
+    ) {
+      setIsFormValid(true);
+    } else {
+      setIsFormValid(false);
+    }
+  };
   useEffect(() => {
     calculatedTotalPrice();
+    checkFormValidity();
   }, [selectedVenue, cateringFacility, designAndPhotoFacility]);
+  useEffect(() => {
+    checkFormValidity();
+  }, [
+    selectedState,
+    selectedCity,
+    selectedVenue,
+    cateringFacility,
+    designAndPhotoFacility,
+    selectedCaterer,
+    selectedDesignServices,
+    selectedPhotoVideoServices,
+    eventDescription,
+    selectedDate,
+    capacity,
+  ]);
+
   return (
-    <section className="pricing">
+    <section className="pricing event-booking-private">
       <div className="events">
-        <div className="private-event">
+        <div className="private-event manage-private-event">
           <h3 className="title">Private Events</h3>
           <h3 className="amount">
             Starting from <span className="col">$500</span>
@@ -111,14 +159,20 @@ const PrivateEventBooking = () => {
           </a>
         </div>
         <div className="private-event-img">
-          <img src={privateImg} alt="private Image"/>
+          <img src={privateImg} alt="private Image" />
         </div>
       </div>
       <div className="parent-private-form">
-      <div className="private-form">
-        <h3>Want to Book Your Own Event? Fill the Details Below</h3>
+        <div className="private-form">
+          <div className="private-form-heading">
+          <h3>
+            Want to Book Your Own Event?
+            
+          </h3>
+          <h3><span className="col">Fill the Details Below</span></h3>
+          </div>
           <form className="private-event-form">
-            <div className="event-select">
+            <div className="event-select facilities">
               <label htmlFor="eventType">Select Event Type:</label>
               <select id="eventType" name="eventType">
                 <option value="">Select Event Type</option>
@@ -129,7 +183,7 @@ const PrivateEventBooking = () => {
                 ))}
               </select>
             </div>
-            <div className="selected-state">
+            <div className="selected-state facilities">
               <label htmlFor="state">Select State:</label>
               <select
                 id="state"
@@ -145,7 +199,7 @@ const PrivateEventBooking = () => {
               </select>
             </div>
             {selectedState && (
-              <div className="selected-city">
+              <div className="selected-city facilities">
                 <label htmlFor="city">Select City</label>
                 <select
                   id="city"
@@ -162,7 +216,7 @@ const PrivateEventBooking = () => {
               </div>
             )}
             {selectedCity && (
-              <div>
+              <div className="selected-venue facilities">
                 <label htmlFor="venue">Select Venue:</label>
                 <select id="venue" onChange={handleVenueChange}>
                   <option value="">Select Venue</option>
@@ -177,8 +231,8 @@ const PrivateEventBooking = () => {
               </div>
             )}
             {selectedVenue && (
-              <div className="venue-details">
-                <div className="venueNamediv">
+              <div className="venue-details facilities">
+                <div className="venueNamediv facilities">
                   <label htmlFor="venueName">Venue Name:</label>
                   <input
                     id="venueName"
@@ -187,7 +241,7 @@ const PrivateEventBooking = () => {
                     disabled
                   />
                 </div>
-                <div className="venueCapacitydiv">
+                <div className="venueCapacitydiv facilities">
                   <label htmlFor="venueCapacity">Capacity:</label>
                   <input
                     id="venueCapacity"
@@ -196,7 +250,7 @@ const PrivateEventBooking = () => {
                     disabled
                   />
                 </div>
-                <div className="venueLocationdiv">
+                <div className="venueLocationdiv facilities">
                   <label htmlFor="venueLocation">Location:</label>
                   <input
                     id="venueLocation"
@@ -205,7 +259,7 @@ const PrivateEventBooking = () => {
                     disabled
                   />
                 </div>
-                <div className="venueContactdiv">
+                <div className="venueContactdiv facilities">
                   <label htmlFor="venueContact">Contact:</label>
                   <input
                     id="venueContact"
@@ -216,7 +270,7 @@ const PrivateEventBooking = () => {
                 </div>
               </div>
             )}
-            <div>
+            <div className="event-description facilities">
               <label htmlFor="eventDescription">Event Description</label>
               <textarea
                 id="eventDescription"
@@ -224,13 +278,25 @@ const PrivateEventBooking = () => {
                 rows="4"
                 cols="50"
                 placeholder="Describe your event..."
+                onChange={(e) => setEventDescription(e.target.value)}
               ></textarea>
             </div>
             {selectedVenue && (
-              <div>
+              <div className="date-input-container facilities">
+                <label htmlFor="date">Select your date of event</label>
+                <input
+                  type="date"
+                  value={selectedDate}
+                  onChange={handleDateChange}
+                  className="date-input"
+                />
+              </div>
+            )}
+            {selectedVenue && (
+              <div className="catering-facility facilities">
                 <label>Do you want our Catering facility?</label>
                 <div>
-                  <label>
+                  <label className="option-select">
                     <input
                       type="radio"
                       name="cateringFacility"
@@ -240,7 +306,7 @@ const PrivateEventBooking = () => {
                     />
                     Yes
                   </label>
-                  <label>
+                  <label className="option-select">
                     <input
                       type="radio"
                       name="cateringFacility"
@@ -252,7 +318,7 @@ const PrivateEventBooking = () => {
                   </label>
                 </div>
                 {cateringFacility === "yes" && selectedVenue && (
-                  <div>
+                  <div className="select-caterer facilities">
                     <label>Select Caterer:</label>
                     <select
                       value={selectedCaterer}
@@ -271,10 +337,10 @@ const PrivateEventBooking = () => {
               </div>
             )}
             {selectedVenue && (
-              <div>
-                <label>Do you want our Design and media facilities?</label>
-                <div>
-                  <label>
+              <div className="design-media">
+                <div className="facilities">
+                  <label>Do you want our Design and media facilities?</label>
+                  <label className="option-select">
                     <input
                       type="radio"
                       name="designandPhotoFacility"
@@ -284,7 +350,7 @@ const PrivateEventBooking = () => {
                     />
                     Yes
                   </label>
-                  <label>
+                  <label className="option-select">
                     <input
                       type="radio"
                       name="designandPhotoFacility"
@@ -296,7 +362,7 @@ const PrivateEventBooking = () => {
                   </label>
                 </div>
                 {designAndPhotoFacility === "yes" && selectedVenue && (
-                  <div>
+                  <div className="facilities">
                     <label>Select Design Service:</label>
                     <select
                       value={selectedDesignServices}
@@ -315,7 +381,7 @@ const PrivateEventBooking = () => {
                   </div>
                 )}
                 {designAndPhotoFacility === "yes" && selectedVenue && (
-                  <div>
+                  <div className="facilities">
                     <label>Select Media Service:</label>
                     <select
                       value={selectedPhotoVideoServices}
@@ -338,23 +404,33 @@ const PrivateEventBooking = () => {
               </div>
             )}
             {selectedVenue && (
-              <div className="capacity">
+              <div className="capacity facilities">
                 <label>Capacity(People)</label>
                 <input
                   type="number"
                   value={capacity}
                   onChange={handleCapacityChange}
+                  placeholder="Enter Capacity"
                 />
               </div>
             )}
+            {selectedVenue && (
+              <div className="submit-btn facilities">
+                <button
+                  type="submit"
+                  className="btn"
+                  disabled={!isFormValid}
+                  style={{
+                    cursor: isFormValid ? "pointer" : "not-allowed",
+                  }}
+                >
+                  Pay Rs: {totalPrice}
+                </button>
+              </div>
+            )}
           </form>
-          {selectedVenue && (
-            <div className="submit-btn">
-              <button type="submit" className="btn">Pay Rs: {totalPrice}</button>
-            </div>
-          )}
         </div>
-        </div>
+      </div>
     </section>
   );
 };
